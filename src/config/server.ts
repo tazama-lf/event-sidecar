6766493dfee64cdcd8fa8a14390066548510ab91
rtkay123 <70331483+rtkay123@users.noreply.github.com>
2024-12-02 16:23:12 +0200

    build: update libs (#130)
    Co-authored-by: rtkay123 <rodney.kanjala@sybrin.com>

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { validateEnvVar } from '@tazama-lf/frms-coe-lib/lib/helpers/env';
+import { validateEnvVar } from '@tazama-lf/frms-coe-lib/lib/config';
 
 export const subject = validateEnvVar<string>('NATS_SUBJECT', 'string');
 export const server = validateEnvVar<string>('NATS_SERVER', 'string');
